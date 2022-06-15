0f427e98e4de218c4560add80c1c22b690406edf
Victoria Ziegler <toriziegler@cox.net>
2022-06-15 12:22:01 -0700

    Feature 5 Complete

--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from receipts.models import Receipt
+
 
 # Create your views here.
+
+
+class ReceiptListView(ListView):
+    model = Receipt
+    template_name = "receipts/list.html"
